53ecf48a754a06bff9327dc3e4692ee9b8fe0f45
Yura <yuralvov754755@gmail.com>
2020-11-19 17:44:40 +0200

    redactions

--- js/main.js
+++ js/main.js
@@ -4,32 +4,16 @@ let sumNumber = 0;
 do{
     minNumber = +prompt('Хей Введи число N');
 }
-while(Number.isInteger(minNumber) === false);
-if(minNumber != null && minNumber != "" && minNumber != false){
-    
-}else {
-   console.log('шось пішло не так ');
-   minNumber--;
-
-}
+while(!Number.isInteger(minNumber));
 do{
     maxNumber = +prompt('Хей Введи число M');
 }
-while(Number.isInteger(maxNumber) === false);
-if(maxNumber != null && maxNumber != "" && minNumber != false){
-    
-}else {
-    console.log('шось пішло не так ');
-    minNumber--;
-
-}
-
+while(!Number.isInteger(maxNumber));
  const userChoice = confirm('Пропустити парні числа?');
 for (let i = minNumber; i <= maxNumber; i++){
 if(userChoice && i%2 === 1){
-    sumNumber += i;
-}
-else if(!userChoice){
+    continue;
+}{
     sumNumber += i;
 }}
 
